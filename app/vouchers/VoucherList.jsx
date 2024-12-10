8c0c9e95a3c1af0ded322dc74c6167a71805cd75
iezhkv <lev3el6up9@gmail.com>
2024-12-10 08:25:27 +0200

    import drama

--- app/vouchers/VoucherList.jsx
+++ app/vouchers/VoucherList.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Design1 from "../components/vouchers/Design1/Design1";
+import Design1 from "@/app/components/vouchers/Design1/Design1";
 import { fetchVouchers } from "@/app/actions/voucherActions";
 
 export const dynamic = "force-dynamic";
